5be5a934afd260453d0d2c8e3376ed3eb07a46ba
Gkthakare <60167550+Gkthakare@users.noreply.github.com>
2022-04-03 14:23:52 +0530

    Notes app

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.notes
 
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -39,13 +44,36 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
     }
 
     override fun onItemClicked(note: Note) {
+//        if (note.text.equals("anupam chutiya", ignoreCase = true)) {
+//            val dialog = AlertDialog.Builder(this)
+//                .setTitle("Universal Truth hai bskd")
+//                .setMessage("Ye baaten chupai nahi jaa skti hain.")
+//                .create()
+//            dialog.apply {
+//                val view = LinearLayout(this@MainActivity).apply {
+//                    addView(TextView(this@MainActivity).apply {
+//                        setText("Anupam Randi")
+//                    })
+                    /*layoutParams = LinearLayout.LayoutParams(
+                        this@MainActivity,
+
+                    ).apply {
+                        width = LinearLayout.LayoutParams.WRAP_CONTENT
+                        height = LinearLayout.LayoutParams.WRAP_CONTENT
+                    }*/
+//                }
+//                setView(view)
+//            }
+//            dialog.show()
+//            return
+//        }
         viewModel.deleteNote(note)
         Toast.makeText(this, "${note.text}Deleted", Toast.LENGTH_LONG).show()
     }
 
     fun submitData() {
         val input = binding.input
-        val noteText = input.text.toString()
+        val noteText = input.text.toString().trim()
         if (noteText.isNotEmpty()) {
             viewModel.insertNote(Note(noteText))
             Toast.makeText(this, "${noteText}Inserted", Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/notes/NotesRVAdapter.kt
+++ app/src/main/java/com/example/notes/NotesRVAdapter.kt
@@ -1,34 +1,46 @@
 package com.example.notes
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 
-class NotesRVAdapter(private val context : Context, private val listener : INotesRVAdapter) : RecyclerView.Adapter<NotesRVAdapter.NoteViewHolder>() {
+class NotesRVAdapter(private val context: Context, private val listener: INotesRVAdapter) :
+    RecyclerView.Adapter<NotesRVAdapter.NoteViewHolder>() {
     private val allNotes = ArrayList<Note>()
 
-    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView = itemView.findViewById<TextView>(R.id.text)
         val deleteButton = itemView.findViewById<ImageView>(R.id.deleteButton)
     }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): NoteViewHolder {
-       val viewHolder = NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.item_note,parent,false))
+        val viewHolder =
+            NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.item_note, parent, false))
         viewHolder.deleteButton.setOnClickListener {
             listener.onItemClicked(allNotes[viewHolder.adapterPosition])
         }
-       return viewHolder
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: NotesRVAdapter.NoteViewHolder, position: Int) {
         val currentNote = allNotes[position]
         holder.textView.text = currentNote.text
+        if (position % 2 == 1) {
+            (holder.itemView as MaterialCardView).setCardBackgroundColor(Color.parseColor("#FEFEFE"));
+            //  holder.imageView.setBackgroundColor(Color.parseColor(""));
+        } else {
+//            (holder.itemView as MaterialCardView).setCardBackgroundColor(Color.parseColor("RED"));
+            holder.textView.setTextColor(Color.parseColor("#FFFFFF"))
+        }
     }
 
     override fun getItemCount(): Int {
@@ -36,7 +48,7 @@ class NotesRVAdapter(private val context : Context, private val listener : INote
 
     }
 
-    fun updateList(newList : List<Note>) {
+    fun updateList(newList: List<Note>) {
         allNotes.clear()
         allNotes.addAll(newList)
 
@@ -45,8 +57,7 @@ class NotesRVAdapter(private val context : Context, private val listener : INote
 }
 
 
-
 interface INotesRVAdapter {
-    fun onItemClicked(note : Note)
+    fun onItemClicked(note: Note)
 
 }
